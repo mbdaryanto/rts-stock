12368b6bd1d698e019e6a4552dbeeaa3f37b95cd
Marcel B. Daryanto <marcel.budiono@gmail.com>
2021-11-01 22:20:19 +0700

    adding client route

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import type { ReactNode } from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
+import { HashRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
+import { AuthProvider, useAuthContext } from './components/auth'
 
-function App() {
+const App = () => (
+  <ChakraProvider>
+    <AuthProvider>
+      <Router>
+        <Switch>
+          <Route path="/login">
+            Login Page
+          </Route>
+          <PrivateRoute path="/" exact>
+            Main Page
+          </PrivateRoute>
+          <PrivateRoute path="/kartu-stok">
+            Kartu Stok
+          </PrivateRoute>
+          <PrivateRoute path="/ringkasan-stok">
+            Ringkasan Stok
+          </PrivateRoute>
+          <Route path="/test">
+            Test
+          </Route>
+        </Switch>
+      </Router>
+    </AuthProvider>
+  </ChakraProvider>
+)
+
+function PrivateRoute({ children, ...rest }: {
+  children: ReactNode,
+  [key: string]: any,
+}) {
+  const { auth } = useAuthContext();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Route
+      {...rest}
+      render={({ location }) =>
+        auth.isAuthenticated ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: location }
+            }}
+          />
+        )
+      }
+    />
   );
 }
 
