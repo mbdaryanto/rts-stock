30b439bea5bba62d9b9ea8c2862596db612c83e0
Marcel B. Daryanto <marcel.budiono@gmail.com>
2021-11-05 17:23:42 +0700

    adding item get and save

--- stock/routers/item.py
+++ stock/routers/item.py
@@ -1,8 +1,8 @@
 from typing import Optional, List
 from fastapi import APIRouter, Depends
-from sqlalchemy import select, and_, or_
+from sqlalchemy import select, and_, or_, update
 from sqlalchemy.orm import Session
-from pydantic import parse_obj_as
+from pydantic import parse_obj_as, BaseModel
 
 from ..db.connection import get_session
 from ..db.schema import Item
@@ -14,7 +14,7 @@ router = APIRouter(
 )
 
 @router.get('/list', response_model=List[ItemModel])
-async def get_list(
+async def get_item_list(
     q: str = '',
     limit: int = 20,
     offset: int = 0,
@@ -25,3 +25,44 @@ async def get_list(
     ).scalars().all()
 
     return parse_obj_as(List[ItemModel], result)
+
+@router.post('/get/{item_id}', response_model=ItemModel)
+async def get_item_by_id(
+    item_id: int,
+    session: Session = Depends(get_session),
+):
+    return session.execute(
+        select(Item).where(Item.id == item_id)
+    ).scalars().one()
+
+class SaveResponse(BaseModel):
+    success: bool = True
+    error: Optional[str] = None
+
+@router.post('/save', response_model=SaveResponse)
+async def save_item(
+    item: ItemModel,
+    session: Session = Depends(get_session),
+):
+    try:
+        if item.id is not None:
+            result = session.execute(
+                update(Item).where(
+                    Item.id == item.id
+                ).values(
+                    **item.dict(exclude={'id'})
+                )
+            )
+            assert result.rowcount == 1, 'Error item not found'
+
+        else:
+            new_item = Item(**item.dict())
+            session.add(new_item)
+
+        session.commit()
+        return SaveResponse()
+
+    except Exception as ex:
+        session.rollback()
+        return SaveResponse(success=False, error=str(ex))
+
