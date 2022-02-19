9e880c7677812adb3cea5c16f643e07d0351cf67
Marcel B. Daryanto <marcel.budiono@gmail.com>
2022-02-19 11:08:45 +0700

    ErrorBoundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,21 @@
-import { Suspense, Component, lazy, ReactNode } from 'react'
+import { Suspense, lazy } from 'react'
 import { ChakraProvider, Center, CircularProgress } from '@chakra-ui/react'
 import { RecoilRoot } from 'recoil'
 import { HashRouter as Router, Routes, Route, Outlet, Navigate, useLocation } from 'react-router-dom'
 import { AuthProvider, useAuthContext } from './components/auth'
 import Navbar from './components/Navbar'
+import ErrorBoundary from './components/ErrorBoundary'
 // import ItemListPage from './pages/ItemList'
 import ItemCategoryPage from './pages/ItemCategory'
 import MarketPlacePage from './pages/MarketPlace'
 
 const ItemListPage = lazy(() => import('./pages/ItemList'))
 
-interface ErrorBoundaryProps {
-  children: ReactNode
-}
-interface ErrorBoundaryState {
-  hasError: boolean
-}
-class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
-  constructor(props: ErrorBoundaryProps) {
-    super(props);
-    this.state = { hasError: false };
-  }
-
-  static getDerivedStateFromError(error: any) {
-    // Update state so the next render will show the fallback UI.
-    return { hasError: true };
-  }
-
-  componentDidCatch(error: any, errorInfo: any) {
-    // You can also log the error to an error reporting service
-    // logErrorToMyService(error, errorInfo);
-  }
-
-  render() {
-    if (this.state.hasError) {
-      // You can render any custom fallback UI
-      return <h1>Something went wrong.</h1>;
-    }
-    return this.props.children;
-  }
-}
+const Loading = (
+  <Center w="100%" h="100vh">
+    <CircularProgress isIndeterminate/>
+  </Center>
+)
 
 const App = () => (
   <ChakraProvider>
@@ -47,13 +23,7 @@ const App = () => (
       <AuthProvider>
         <Router>
           <ErrorBoundary>
-            <Suspense fallback={(
-              <Navbar title="Stock App">
-                <Center w="100%" h="200px">
-                  <CircularProgress isIndeterminate/>
-                </Center>
-              </Navbar>
-            )}>
+            <Suspense fallback={Loading}>
               <Routes>
                 <Route path="/" element={<LayoutPage/>}>
                   <Route index element={'Main Page'}/>
---
+++ src/components/ErrorBoundary.tsx
@@ -0,0 +1,35 @@
+import type { ReactNode } from 'react'
+import { Component } from 'react'
+
+interface ErrorBoundaryProps {
+  children: ReactNode
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean
+}
+
+export default class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error: any) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: any, errorInfo: any) {
+    // You can also log the error to an error reporting service
+    // logErrorToMyService(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return <h1>Something went wrong.</h1>;
+    }
+    return this.props.children;
+  }
+}
