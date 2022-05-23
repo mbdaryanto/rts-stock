041ca63bba118459d6fc04bf844dfa6216402b71
Marcel B. Daryanto <marcel.budiono@gmail.com>
2022-05-23 21:44:38 +0700

    change path for index

--- stock/main.py
+++ stock/main.py
@@ -1,8 +1,9 @@
-from os.path import join, dirname, abspath, exists
+from pathlib import Path
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 from .routers import item, market_place, purchase
+# from .settings import get_settings
 
 
 app = FastAPI()
@@ -11,22 +12,26 @@ app.include_router(item.router)
 app.include_router(market_place.router)
 app.include_router(purchase.router)
 
-STATIC_FILES_DIR = join(dirname(abspath(__file__)), 'assets')
-if not exists(STATIC_FILES_DIR):
-    STATIC_FILES_DIR = join(dirname(dirname(abspath(__file__))), 'public')
+static_files_dir = Path(__file__).parent / 'assets'
+if not static_files_dir.exists():
+    static_files_dir = Path(__file__).parent.parent / 'public'
 
-app.mount('/assets', StaticFiles(directory=STATIC_FILES_DIR), name='assets')
+app.mount('/assets', StaticFiles(directory=static_files_dir), name='assets')
 
 
-@app.get('/', response_class=HTMLResponse)
+@app.get('/')
 def get_index():
-    filename = join(dirname(abspath(__file__)), 'assets', 'index.html')
-    if not exists(filename):
-        filename = join(dirname(dirname(abspath(__file__))), 'public', 'index.html')
-    with open(filename) as f:
-        return HTMLResponse(f.read())
+    index_html = static_files_dir / 'index.html'
+    if not index_html.exists():
+        return "Welcome to Stock"
+    return FileResponse(index_html)
 
 
-@app.get("/hello", response_class=HTMLResponse)
+@app.get("/hello")
 async def get_hello():
     return "Hello World"
+
+
+# @app.get('/dbs')
+# def get_dbs():
+#     return get_settings().get_db_url().render_as_string()
